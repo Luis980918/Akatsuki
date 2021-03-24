446c81c91af4762456b3b8492a7b4c9dc313c2b9
Luis Carlos <lcarlos.rendon@udea.edu.co>
2021-03-23 20:04:23 -0500

    Proyecto de Gradle hecho en clase por Luis Carlos Rendon

---
+++ Fuentes/ProyectoJsonLuis_Carlos_Rendon/src/main/java/Adress.java
@@ -0,0 +1,38 @@
+public class Adress {
+    public String getStreedAdress() {
+        return streedAdress;
+    }
+
+    public void setStreedAdress(String streedAdress) {
+        this.streedAdress = streedAdress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    private String streedAdress;
+    private String city;
+    private String state;
+    private String postalCode;
+}
